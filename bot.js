a32e865b6114439e6e37f3cf3b679eed13696a32
Chandler Davis <chandler@photonfighterlabs.com>
2018-12-15 22:17:59 -0500

    Basic bot functionality now enabled.

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+var Discord = require('discord.io'); // Discord Library
+var logger = require('winston'); // Logger library
+var auth = require('./auth.json'); // how we access our auth token
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -15,25 +15,34 @@ var bot = new Discord.Client({
   autorun: true
 });
 
+//When the bot is ready, do this stuff
 bot.on('ready', function(evt) {
+  // Prints to command line that the bot is running off of
   logger.info('Connected');
   logger.info('Logged in as: ');
   logger.info(bot.username + '-(' + bot.id + ')');
 });
-bot.on('message', function(user, userID, channelID, message, evt) {
-  // Our bot needs to know if it will execute a command
-  // It will listen for messages that start with '!', for know
-  if(message.substring(0,1)=='!') {
-    var args = message.substring(1).split('');
-    var cmd = args[0];
 
-    args = args.splice(1);
-    switch(cmd) {
+// Do this when the bot receives a message
+bot.on('message', function(user, userID, channelID, message, evt) {
+  logger.info('Message received:' + message);
+
+  // Our bot needs to know if it will execute a command
+  // It will listen for messages that start with '!', for now
+  if(message.substring(0,1)=='!') {
+    logger.info("'!' detected!");
+
+    // Finds a string that starts with !
+    var args = message.substring(1).split('');
+    args = args.join('');
+
+    switch(args) {
       //!ping
 
       case 'ping':
+        logger.info('ping detected!');
         bot.sendMessage({
-          to: 'botTest',
+          to: channelID,
           message: 'Pong!'
         });
         break;
